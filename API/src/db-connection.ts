d8678075202c99a0fa678cd3374deeb4359434d2
Juan Leiton <jgleitonl@gmail.com>
2023-04-13 14:23:35 -0500

    mongodb connection setup

---
+++ API/src/db-connection.ts
@@ -0,0 +1,20 @@
+import * as dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
+mongoose.set('strictQuery', true)
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env
+const validEnv = typeof DB_USER === 'string' && typeof DB_PASSWORD === 'string' && typeof DB_HOST === 'string' && typeof DB_NAME === 'string'
+// const validEnv = [DB_USER, DB_PASSWORD, DB_HOST, DB_NAME]
+//   .map(env => typeof env)
+//   .every(type => type === 'string')
+const connection = validEnv ? `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}` : 'invalid connection'
+
+mongoose.connect(connection)
+  .then(() => {
+    console.log('SilkRoad is connected to ', DB_NAME)
+  })
+  .catch(error => {
+    console.error('Failed connection', error.message)
+  })
