d5f0640662fec2d0690738ea042d42b26cf27683
Juan Leiton <jgleitonl@gmail.com>
2023-04-21 17:43:42 -0500

    GET /defaultFeed route defined

--- API/src/routes/productRoutes.ts
+++ API/src/routes/productRoutes.ts
@@ -1,10 +1,22 @@
 import express from 'express'
 import cors from 'cors'
+import { getDefaultFeed } from '../controllers/productControllers'
 
 const productRouter = express.Router()
 
-productRouter.get('/product', cors(), (_req, res) => {
-  res.send('All products')
+productRouter.get('/defaultFeed/:numItems/:page', cors(), (req, res) => {
+  try {
+    const { numItems, page } = req.params
+    const itemsPerPage = Number(numItems)
+    const currentPage = Number(page)
+    getDefaultFeed(itemsPerPage, currentPage).then(result => {
+      res.json(result)
+    }).catch(() => {
+      console.log('Not found')
+    })
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 export default productRouter
