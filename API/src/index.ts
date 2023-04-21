b5f8a8b854bfc86c438c48b55c6d584155ec49f6
Juan Leiton <jgleitonl@gmail.com>
2023-04-21 17:36:54 -0500

    Server connection on deployment ports enabled

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,18 +1,27 @@
 import express from 'express'
+import morgan from 'morgan'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import productRouter from './routes/productRoutes'
 
-dotenv.config()
-const { SERVER_PORT } = process.env
-const Port = SERVER_PORT !== undefined ? SERVER_PORT : 3001
-
 const app = express()
+
+// env variables
+dotenv.config()
+const { LOCAL_SERVER_PORT } = process.env
+
+// settings
+app.set('port', process.env.PORT ?? LOCAL_SERVER_PORT)
+
+// middlewares
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(morgan('dev'))
 app.use(cors())
+
+// routes
 app.use('/', productRouter)
 
-app.listen(Port, () => {
-  console.log(`Listening at http://localhost:${Port}`)
+app.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'))
 })
