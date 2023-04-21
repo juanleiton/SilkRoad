73e4d905cf4b9f9fab6764dedbcb97efef578d91
Juan Leiton <jgleitonl@gmail.com>
2023-04-21 17:44:59 -0500

    getDefaultFeed controller working

--- API/src/controllers/productControllers.ts
+++ API/src/controllers/productControllers.ts
@@ -3,10 +3,18 @@ import Product from '../schemas/productSchema'
 import { IProduct } from '../definitions/interfaces'
 
 export async function getDefaultFeed (itemsPerPage: number, currentPage: number):
-Promise<IProduct[] | []> {
-  const defaultFeed = (await Product.find({})
-    .sort({ updatedAt: 'desc' }))
-    .slice(itemsPerPage * (currentPage - 1)) // An aux function can be defined and then imported for all routes
-    .slice(0, itemsPerPage - 1)
+Promise<IProduct[]> {
+  console.log(itemsPerPage, currentPage)
+  const defaultFeed = await Product.find({})
+    // .populate('User')
+    // .populate('Category')
+    // .populate('Subcategory')
+    // .populate('Brand')
+    // .populate('Review')
+    // .populate('Question')
+    .sort({ updatedAt: 'desc' })
+    .skip(itemsPerPage * (currentPage - 1)) // An aux function can be defined and then imported for all routes
+    .limit(itemsPerPage)
+  console.log(defaultFeed)
   return defaultFeed
 }
