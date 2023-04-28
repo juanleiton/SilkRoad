9f20aa27913de6bea6aa48270056fcb1190f1773
Juan Leiton <jgleitonl@gmail.com>
2023-04-27 19:28:30 -0500

    Interfaces purged

--- Client/src/definitions/interfaces.ts
+++ Client/src/definitions/interfaces.ts
@@ -1,37 +1,29 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 
-export interface Iresolve {
-  extensions: string[]
-}
-
-export interface Ioutput {
-  filename: string
-  path: string
-}
-
 export interface Irule {
   test: RegExp
   exclude?: RegExp
 }
 
-export interface Imodule {
-  rules: Irule[]
-}
-
-export interface Idevserver {
-  open: boolean
-  hot: boolean
-  liveReload: boolean
-  static: string[]
-  port: number
-  compress: boolean
-}
-
 export interface IWebpackConfig {
   entry: String
-  resolve: Iresolve
-  output: Ioutput
+  resolve: {
+    extensions: string[]
+  }
+  output: {
+    filename: string
+    path: string
+  }
   plugins: HtmlWebpackPlugin[]
-  module: Imodule
-  devServer: Idevserver
+  module: {
+    rules: Array<{ test: RegExp, exclude?: RegExp }>
+  }
+  devServer: {
+    open: boolean
+    hot: boolean
+    liveReload: boolean
+    static: string[]
+    port: number
+    compress: boolean
+  }
 }
